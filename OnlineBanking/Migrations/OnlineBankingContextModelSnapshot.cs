ee6552c1604b1804dcd6b004f5ff278beb94fcce
Huy Anh <ahuy631996@gmail.com>
2021-06-25 20:57:44 +0700

    Add User area add User index

--- OnlineBanking/Migrations/OnlineBankingContextModelSnapshot.cs
+++ OnlineBanking/Migrations/OnlineBankingContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace OnlineBanking.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.7")
+                .HasAnnotation("ProductVersion", "5.0.6")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("OnlineBanking.Models.Account", b =>
--- OnlineBanking/Startup.cs
+++ OnlineBanking/Startup.cs
@@ -54,6 +54,14 @@ namespace OnlineBanking
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
