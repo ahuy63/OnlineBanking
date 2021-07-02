a01a90c1050bb6a649eb2b1dd09eef273b4c9d05
ptp-phamphong <74300269+ptp-phamphong@users.noreply.github.com>
2021-07-02 20:21:20 +0700

    Commit để pull về, git khó vãi

--- OnlineBanking/Areas/UserSection/Controllers/SendController.cs
+++ OnlineBanking/Areas/UserSection/Controllers/SendController.cs
@@ -39,6 +39,8 @@ namespace OnlineBanking.Areas.UserSection.Controllers
             //Lấy danh sách người nhận đem qua bên view
             ViewBag.AllAddressBookById = _context.AddressBooks.Include(ad => ad.Account).Include(ad => ad.Account.User).Include(ad => ad.User).Where(ad => ad.UserId == HttpContext.Session.GetInt32("IdCurrentUser"));
 
+            //Lấy 1 người nhận default
+
             //Lấy danh sách tài khoản đem qua bên view
             ViewBag.AccountList = _context.Accounts.Include(acc => acc.User).Include(acc =>acc.AccountType).Where(acc => acc.UserId == HttpContext.Session.GetInt32("IdCurrentUser"));
 
