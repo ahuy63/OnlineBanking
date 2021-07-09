55fee457461229c709fd94acf918db728f2fa468
Huy Anh <ahuy631996@gmail.com>
2021-07-09 19:18:06 +0700

    AdminUser Layout

---
+++ OnlineBanking/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineBanking.Data;
+using OnlineBanking.Models;
+
+namespace OnlineBanking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountsController : Controller
+    {
+        private readonly OnlineBankingContext _context;
+
+        public AccountsController(OnlineBankingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Accounts
+        public async Task<IActionResult> Index()
+        {
+            var onlineBankingContext = _context.Accounts.Include(a => a.AccountType).Include(a => a.User);
+            return View(await onlineBankingContext.ToListAsync());
+        }
+
+        // GET: Admin/Accounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.AccountType)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Create
+        public IActionResult Create()
+        {
+            ViewData["AccountTypeId"] = new SelectList(_context.AccountTypes, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,AccountTypeId,Number,Balance,CreateDate,Status")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountTypeId"] = new SelectList(_context.AccountTypes, "Id", "Name", account.AccountTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", account.UserId);
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountTypeId"] = new SelectList(_context.AccountTypes, "Id", "Name", account.AccountTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", account.UserId);
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,AccountTypeId,Number,Balance,CreateDate,Status")] Account account)
+        {
+            if (id != account.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountTypeId"] = new SelectList(_context.AccountTypes, "Id", "Name", account.AccountTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", account.UserId);
+            return View(account);
+        }
+
+        // GET: Admin/Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.AccountType)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Admin/Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.Accounts.Any(e => e.Id == id);
+        }
+    }
+}
